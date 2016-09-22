74f67a3fe54412a6976b5f9817fde5aaab1eed9b
alenkalac <alenkalac87@gmail.com>
2016-09-22 18:02:34 +0100

    Homepage Finished

--- index.php
+++ index.php
@@ -2,15 +2,34 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>Welcome!</title>
+		<title>Portfolio</title>
 		<style>
 			@import "css/main.css";
 		</style>
 		<script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="   crossorigin="anonymous"></script>
 	</head>
 	<body>
-		<div class="">
-			
+		<div class="container">
+			<nav class="main-nav">
+				<ul>
+					<li class="logo">AK</li>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">My Work</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</nav>
+			<div class="main-body">
+				<h1 class="title">Welcome! My name is Alen</h1>
+				<span class="subtitle">And I am a PHP & HTML Developer.</span>
+				<a href="/work.php" class="CTA">Check My Work</a>
+			</div>
+
+			<div class="s-media">
+				<a href="https://ie.linkedin.com/in/alenkalac" target="_blank"><i class="ion-social-linkedin"></i></a>
+				<a href="https://github.com/alenkalac" target="_blank"><i class="ion-social-github"></i></a>
+				<a href="https://twitter.com/ExtraNullByte" target="_blank"><i class="ion-social-twitter"></i></a>
+				<a href="http://facebook.com/alenkalac" target="_blank"><i class="ion-social-facebook"></i></a>
+			</div>
 		</div>
 	</body>
 </html>
